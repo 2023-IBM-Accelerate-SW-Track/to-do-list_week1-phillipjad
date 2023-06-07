8aa4f3136a61cd25f2e5dd9775050a34e501a9a2
Phillip Dickey <theraidnade@gmail.com>
2023-06-07 19:52:22 -0400

    added cool stuff

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import "./About.css";
+import pfp from "../assets/IMG_1593.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+         <div>
+        <div className="split left">
+          <div className="centered">
+            <img 
+              className="profile_image" src={pfp}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Phillip Dickey</div>
+            <div className="brief_description">
+              I like turtles
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   }
